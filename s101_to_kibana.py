b91d9a1401bb67df6414cfc008c52bd3bd7dbefa
Marcio Marchini <mqm@sglebs.com>
2015-11-23 08:23:35 -0200

    lowercase for kibana

--- s101_to_kibana.py
+++ s101_to_kibana.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     structure_url = arguments["--structureURL"]
     es = Elasticsearch([elastic_search_url])
     uname = platform.uname()
-    es.index(index="s101_to_kibana", doc_type="test-type",
+    es.index(index="s101-runner", doc_type="run",
              body={"author": "%s@%s" % (getpass.getuser(), platform.node()),
                     "date": datetime.datetime.now().isoformat(),
                     "arguments": arguments
@@ -33,19 +33,22 @@ if __name__ == '__main__':
     input_xml_stream = http.urlopen('GET',structure_url)
     s101_data_as_xml = ET.ElementTree(file=input_xml_stream)
     chart = s101_data_as_xml.getroot()
+    chart_name = chart.attrib["name"].lower()  # e.g. "Size"
     #print(root)
     for series in chart.getchildren():
+        series_name = series.attrib["name"]  # e.g. PG5_MAIN
         #print(series.attrib)
         for entry in series:
             #print(entry.attrib)
-            body = {"series_name": series.attrib["name"]}
+            body = {"series_name": series_name}
             attribs = entry.attrib
             if attribs["time"] == "":
                 del attribs["time"] #no point in having empty data
             entry_date_as_string = attribs["date"]
             entry_date = datetime.datetime.strptime(entry_date_as_string, arguments["--dateFormat"])
             attribs["date"] = entry_date.isoformat()
+            attribs[chart_name] = int(attribs["value"])  # it is an integer attribute
+            del attribs["value"] #no point in having original data
             body.update(attribs)
             print(body)
-            print("---")
-            es.index(index="s101_to_kibana", doc_type="test-type", body=body)
+            es.index(index="s101-%s" % chart_name, doc_type=chart_name, body=body)
